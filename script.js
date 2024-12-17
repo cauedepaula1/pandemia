29e0eeeccd4f4fd1cc6893d917a50710f6463dcb
cauedepaula1 <cauedepaula707@gmail.com>
2024-12-17 18:37:47 -0300

    MaisJs

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ window.addEventListener("scroll", function () {
 // setTimeout
 var res2 = window.document.querySelector(".texto")
 var text2 = "Pandemia e afeto";
-var interval2 = 150;
+var interval2 = 130;
 
 //para digitar o texto
 function digito1(element2, text2, interval2){
@@ -45,3 +45,26 @@ function digito1(element2, text2, interval2){
 window.addEventListener("load", function () {
     digito1(res2, text2, interval2);
 });
+var res3 = window.document.getElementById("paragrafogrid")
+var text3 = "Diferentes formas de cuidar";
+var interval3 = 430;
+
+function digito3(element3, text3, interval3){
+    var char = text3.split("").reverse();
+    var typer = setInterval(function (){
+        if(!char.length){
+            return clearInterval(typer);
+        }
+        var next = char.pop();
+        element3.innerHTML += next;
+    }, interval3)
+}
+window.addEventListener("scroll", function(){
+    if(res3 && !res3.classList.contains("typed")){
+        res3.classList.add("typed"); // Adiciona uma classe para evitar múltiplas execuções
+        digito3(res3, text3, interval3) //chama variaveis
+    }
+});
+
+
+
